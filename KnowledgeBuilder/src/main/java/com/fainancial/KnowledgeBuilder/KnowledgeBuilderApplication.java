a346c6b41eaccceec1661866cb3494860c769038
Zeeshan Akhtar <zshanakhtar@outlook.com>
2025-07-05 18:23:33 +0000

    init kb

---
+++ KnowledgeBuilder/src/main/java/com/fainancial/KnowledgeBuilder/KnowledgeBuilderApplication.java
@@ -0,0 +1,13 @@
+package com.fainancial.KnowledgeBuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowledgeBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnowledgeBuilderApplication.class, args);
+	}
+
+}
