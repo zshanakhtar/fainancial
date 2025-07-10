61af8a0d2667193dded974e843b8e1af817250b8
Aman Shakya <amanqait@gmail.com>
2025-07-10 19:38:44 +0530

    rule scraping and save into file

---
+++ KnowledgeBuilder/src/main/java/com/fainancial/extraction/locators/IncomeTaxLocators.java
@@ -0,0 +1,10 @@
+package com.fainancial.extraction.locators;
+
+public class IncomeTaxLocators {
+    public final String totalRecords = "div.act_search_footer span";
+    public final String ruleTitle = "h3.search_title";
+    public final String ruleTitleLink = "h3.search_title a";
+    public final String inputPageNumber = "div.act_search_footer span input[name*='txtPageNumber']";
+    public final String ruleDialogContent = "viewerContent";
+    public final String ruleDialogFrame = "ms-dlgFrame";
+}
